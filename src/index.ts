642cf91840ad5dcfc50a930fcac55153a0b6444c
Ozal Khan <ozalkhan196@gmail.com>
2020-04-13 14:23:46 -0400

    move csv reading logic to own file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
 
-const matches = fs
-  .readFileSync('football.csv', { encoding: 'utf-8' })
-  .split('\n')
-  .map((row: string): string[] => row.split(','));
+const reader = new CsvFileReader('football.csv');
 
-console.log(matches);
+reader.read();
+let manUnitedWins = 0;
+
+// for small set of closely related values for example choice a or choice b
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
+reader.data.forEach((row: string[]): void => {
+  if (row[1] === 'Man United' && row[5] === MatchResult.HomeWin)
+    manUnitedWins++;
+  else if (row[2] === 'Man United' && row[5] === MatchResult.AwayWin)
+    manUnitedWins++;
+});
+
+// console.log(reader.data);
+console.log(manUnitedWins, '<><><>');
